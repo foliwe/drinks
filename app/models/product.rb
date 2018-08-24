a7f6eba0c481fd1362265f8b89af24bda5cbc9bb
foliwe <foliwe@gmail.com>
2018-08-24 03:01:29 +0100

    fre sdsd

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
+  before_save :capitalize_name
   belongs_to :product_category
+  validates :name ,presence: true, uniqueness: { case_sensitive: false }
+
+  def capitalize_name
+    self.name.capitalize!
+  end
+  
 end
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,3 +1,9 @@
 class ProductCategory < ApplicationRecord
+  before_save :capitalize_name
   has_many :products
+  validates :name ,presence: true, uniqueness: { case_sensitive: false }
+
+  def capitalize_name
+    self.name.capitalize!
+  end
 end
