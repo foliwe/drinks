5249792fdb9b1dd7efa271cfadd5d464d156b7a4
foliwe <foliwe@gmail.com>
2018-08-26 02:03:51 +0100

    assest

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module FrozenMeat
     config.application_name = Rails.application.class.parent_name
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
